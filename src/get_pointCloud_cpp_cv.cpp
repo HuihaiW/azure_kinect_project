33dd4c0389be061e0e342bab04a55df610a25bd2
HuihaiW <hwang164@binghamton.edu>
2022-11-05 13:01:10 -0500

    opencv

--- src/get_pointCloud_cpp_cv.cpp
+++ src/get_pointCloud_cpp_cv.cpp
@@ -20,8 +20,6 @@ using namespace Eigen;
 
 typedef Eigen::Matrix<double, 6, 1> Vector6d;
 void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &pointcloud);
-void showPointCloud(const Mat pointcloud, const Mat rgb);
-
 
 int main(int argc, char** argv){
     const uint32_t device_count = k4a::device::get_installed_count();
@@ -141,6 +139,17 @@ int main(int argc, char** argv){
     cout << "R3: " << R3.transpose() << endl;
     translation = rotation_matrix.inverse()*translation;
     cout << "translation: " << translation.transpose() << endl;
+    Mat cv_Rotation(3, 4, CV_64FC1);
+    for(int i=0; i<3; i++){
+	cout << "rotation" << i << endl;
+	for(int j=0; j<4; j++){
+	    if(j=3){
+		cv_Rotation.at<float>(i,j)= -1 * translation(i,0);
+	    }
+	    else
+	        cv_Rotation.at<float>(i,j)=rotation_matrix.inverse()(i,j);
+	}
+    }
 
 
     
@@ -236,15 +245,40 @@ int main(int argc, char** argv){
 	    cv_location = cv::Mat(xyzImage.get_height_pixels(),xyzImage.get_width_pixels(),  CV_16SC3, (void *)xyzImage.get_buffer());
 	    cv_location = cv_location.reshape(1, xyzImage.get_height_pixels()*xyzImage.get_width_pixels() );
 	    cv_rgb = cv_rgbImage_no_alpha.reshape(1, cv_rgbImage_no_alpha.rows*cv_rgbImage_no_alpha.cols);
-	    //cout << "shape: " << cv_location.rows << " x " << cv_location.cols  << endl;
+
+	    Mat col_ones = Mat::ones(pointcount, 1, CV_16SC1);
+	    hconcat(cv_location, col_ones, cv_location);
+
+	    cv_location = cv_location.t();
+	    cv_rgb = cv_rgb.t();
+
+	    cv_location = cv_Rotation * cv_location;
+	    cout << "shape: " << cv_location.rows << " x " << cv_location.cols  << endl;
 	    
 	    for(size_t i=0; i<pointcount; i++){
 		//cout << cv_location.at<double>(0, i) << " ; "  <<cv_location.at<int>(0,i) << endl;
 		
-		//cout << cv_location.at<short>(i, 0)<< " ; " << cv_location.at<short>(i,1) << " ; " << cv_location.at<short>(i,2) << " ; "  <<pointsLocation(i,0) << endl;
+		cout << cv_location.at<short>(i, 0)<< " ; " << cv_location.at<short>(i,1) << " ; " << cv_location.at<short>(i,2) << " ; "  << endl;
 
+		double x = cv_location.at<short>(0, i)/1000.0;
+		double y = cv_location.at<short>(1, i)/1000.0;
+		double z = cv_location.at<short>(2, i)/1000.0;
+		int r = cv_rgb.at<uchar>(0, i);
+		int g = cv_rgb.at<uchar>(1, i);
+		int b = cv_rgb.at<uchar>(2, i);
+		Vector6d point;
+		
+		point[0] = x;
+		point[1] = y;
+		point[2] = z;
+		point[3] = b;
+		point[4] = g;
+		point[5] = r;
+		
+		pointcloud.push_back(point);
 	    }
 	    
+	    showPointCloud(pointcloud);
 	    
 
 	    //int16_t new_buffer[xyz_height][3];
@@ -254,15 +288,24 @@ int main(int argc, char** argv){
 
 
 	    //create image with size of 1280X720 (width, height)
-	    Mat generated_image(360, 640, CV_8UC3, Scalar(0,0,0));
-	    for(size_t i = 0; i < pointcount; i++){
+	    //Mat generated_image(360, 640, CV_8UC3, Scalar(0,0,0));
+	    /*for(size_t i = 0; i < pointcount; i++){
+		float x = static_cast<float>(point_cloud_buffer[3 * i + 0]);
+		float y = static_cast<float>(point_cloud_buffer[3 * i + 1]);
 		float z = static_cast<float>(point_cloud_buffer[3 * i + 2]);
-		if (z <= 0.0 || z ==0){
-		    continue;
-		
-		}
+		//double z=pointsLocation(i,2);
+		//if (z <= 0.0 || z ==0){
+		//    continue;
+		//
+		//}
+		if (z==100000000000000) continue;
 		else{
+		    //cout << colors(i, 0) << " ; " << colors(i, 1) << " ; " << colors(i, 2) << endl;
 
+		    cout <<"opecv MAT: " << cv_location.at<short>(i, 0)<< " ; " << cv_location.at<short>(i,1) << " ; " << cv_location.at<short>(i,2) << "  "  << "slow: " <<  x << " ; " << y << " ; " << z<< endl;
+
+		    
+		    continue;
 		    Vector6d point;
 		    constexpr float kMillimeterToMeter = 1.0/1000.0f;
 		    float x = kMillimeterToMeter * static_cast<float>(point_cloud_buffer[3 * i + 0]);
@@ -272,13 +315,16 @@ int main(int argc, char** argv){
 		    z = kMillimeterToMeter * z;
 		    x = x;
 		    y = y;
+		    //cout << x << " , " << y << " , " << z << " , " << endl;
 
 		    int r = color_buffer[4 * i + 2];
 		    int g = color_buffer[4 * i + 1];
 		    int b = color_buffer[4 * i + 0];
 
-		    Matrix<float, 3, 1> o_p, new_p, image_temp, image_p;
-		    o_p << x, y, z;
+		    //Matrix<float, 3, 1> o_p, new_p, image_temp, image_p;
+		    //o_p << x, y, z;
+		    */
+		    /*
 		    //cout << "new point is: " << rotation_matrix* o_p.cast<double> << endl;
 		    new_p = rotation_matrix.inverse().cast<float>() * o_p;
 		    float image_temp_x = new_p(0, 0) - translation(0, 0) * kMillimeterToMeter;
@@ -292,14 +338,15 @@ int main(int argc, char** argv){
 		    int image_x = static_cast<int>(image_p(0, 0)/2);
 		    int image_y = static_cast<int>(image_p(1, 0)/2);
 
-		    //if(image_y<0) continue;
-		    //if(image_x<0) continue;
-		    //if(image_x>640) continue;
-		    //if(image_y>360) continue;
+		    if(image_y<0) continue;
+		    if(image_x<0) continue;
+		    if(image_x>640) continue;
+		    if(image_y>360) continue;
 		    
-		    //generated_image.at<Vec3b>(image_y, image_x)[0] = b;
-		    //generated_image.at<Vec3b>(image_y, image_x)[1] = g;
-		    //generated_image.at<Vec3b>(image_y, image_x)[2] = r;
+		    generated_image.at<Vec3b>(image_y, image_x)[0] = b;
+		    generated_image.at<Vec3b>(image_y, image_x)[1] = g;
+		    generated_image.at<Vec3b>(image_y, image_x)[2] = r;
+		    */
 
 		    /*
 		     * image_p is the generated image from points, three values are x, y, z location of point in image
@@ -307,16 +354,25 @@ int main(int argc, char** argv){
 		     * new_p is the point cloud converted perpenticularly to the ground.
 		     */
 
-		    //float image_x = image_p(0, 0);
-		    //float image_y = image_p(1, 0);
+		    //The following codes are used to show point clouds
+		    /*
+		    float image_x = image_p(0, 0);
+		    float image_y = image_p(1, 0);
 
+		    if(image_y<0) continue;
+		    if(image_x<0) continue;
+		    if(image_x>1280) continue;
+		    if(image_y>720) continue;
+
+		    //cout << "x, y, u, v: " << image_temp(0, 0) << " ;" << image_temp(1, 0) << " ;" << image_x << " ;" << image_y << endl;
 
 		    
 		    float scale = 1.0;
 		    
-		    point[0] = x;
-		    point[1] = y;
-		    point[2] = z;
+		    point[0] = image_x;
+		    point[1] = image_y;
+		    point[2] = e_z;
+		    */
 		   /* 
 		    point[0] = scale * new_p(0, 0);
 		    point[1] = scale * new_p(1, 0);
@@ -329,11 +385,11 @@ int main(int argc, char** argv){
 		    point[2] = 0;
 		    */
 		    
-		    point[3] = r;
-		    point[4] = g;
-		    point[5] = b;
+		    //point[3] = r;
+		    //point[4] = g;
+		    //point[5] = b;
 
-		    pointcloud.push_back(point);
+		    //pointcloud.push_back(point);
 		   
 		    
 		    /*
@@ -348,13 +404,11 @@ int main(int argc, char** argv){
 		    //point[2] = z;
 		    //pointcloud.push_back(point);
 		    
-          	}
-	    }
+	//	}
+	    //}
 	    
 
 	    //showPointCloud(cv_location, cv_rgb);
-	    cout << "show points" << endl;
-	    showPointCloud(pointcloud);
 	    //
 	    //Mat resized_down;
 	    //resize(generated_image, resized_down, Size(180, 320), INTER_LINEAR);
@@ -414,7 +468,6 @@ void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &
         glPointSize(2);
         glBegin(GL_POINTS);
         for (auto &p: pointcloud) {
-	    //cout << "pointing" << endl;
             glColor3f(p[3]/255.0, p[4]/255.0, p[5]/255.0);
             glVertex3d(p[0], p[1], p[2]);
         }
@@ -423,38 +476,3 @@ void showPointCloud(const vector<Vector6d, Eigen::aligned_allocator<Vector6d>> &
         usleep(5000);   // sleep 5 ms
     }
 }
-/*void showPointCloud(const Mat pointcloud, const Mat rgb) {
-
-    pangolin::CreateWindowAndBind("Point Cloud Viewer", 3840, 2160);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    pangolin::OpenGlRenderState s_cam(
-        pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
-        pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
-    );
-
-    pangolin::View &d_cam = pangolin::CreateDisplay()
-        .SetBounds(0.0, 1.0, pangolin::Attach::Pix(175), 1.0, -1024.0f / 768.0f)
-        .SetHandler(new pangolin::Handler3D(s_cam));
-
-    while (pangolin::ShouldQuit() == false) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        d_cam.Activate(s_cam);
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-
-        glPointSize(2);
-        glBegin(GL_POINTS);
-        for (int i=0; i++; i<pointcloud.rows) {
-	    cout << "start pointing points"<<endl;
-            glColor3f(rgb.at<uchar>(i,0)/255.0, rgb.at<uchar>(i,1)/255.0, rgb.at<uchar>(i,2)/255.0);
-//            glVertex3d(pointcloud.at<short>(i,0)/1000,pointcloud.at<short>(i,1)/1000,pointcloud.at<short>(i,2)/1000);
-            glVertex3d(i, i, i);
-        }
-        glEnd();
-        pangolin::FinishFrame();
-        usleep(5000);   // sleep 5 ms
-    }
-}*/
